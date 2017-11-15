8c301a4244d5227354a008a244705c5955d2ff7a
Chris Spooner <cmspooner@gmail.com>
2017-11-15 15:05:49 -0500

    First Commit it works!

---
+++ app/index.js
@@ -0,0 +1,33 @@
+/*
+ * Entry point for the watch app
+ */
+import document from "document";
+import { display } from "display";
+
+let background = document.getElementById("background");
+let toggle = true;
+
+background.onclick = function(evt) {
+  console.log("Click");
+  if (toggle){
+    toggle = false;
+    background.style.fill = "gray";
+    display.brightnessOverride = undefined;
+    display.autoOff = true;
+  } else{
+    toggle = true;
+    background.style.fill = "white";
+    display.brightnessOverride = 1.0;
+    display.autoOff = false;
+  }
+  console.log("Toggle is " + toggle);
+}
+
+toggle = true;
+background.style.fill = "white";
+display.brightnessOverride = 1.0;
+display.autoOff = false;
+
+display.poke(); 
+
+console.log("App Started");
